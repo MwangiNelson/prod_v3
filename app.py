4c9a7f8b0dc825c54ed1e7ecead4389ac8d2e064
MwangiNelson <nelsonmwangi197@gmail.com>
2024-02-01 02:45:48 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+import requests
+import os
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/generate-text', methods=['POST'])
+def generate_text():
+    input_text = request.json.get('text')
+    response = requests.post(
+        "https://api-inference.huggingface.co/models/MwangiNelson/NutriBotPrivate",
+        headers={"Authorization": "Bearer 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"},
+        json={"inputs": input_text, "parameters": {"max_length": 50}}
+    )
+    if response.status_code == 200:
+        return jsonify(response.json()[0]['generated_text'])
+    else:
+        return jsonify({"error": response.text}), response.status_code
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
